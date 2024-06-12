e522a5cd3daf3a53326122690b63e1b702d18a43
Emanuels97 <ema.mendo97@gmail.com>
2024-06-12 18:47:56 -0300

    Creacion de el tp6

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const { mongoose } = require('./database');
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:4200' }));
+
+// Routes
+// app.use('/api/agente', require('./routes/agente.route.js'));
+// app.use('/api/sector', require('./routes/sector.route'));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Setting
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+// Starting the server
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
